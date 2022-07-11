16ecb922b1097d63a6b79de0733ff337b78b90ae
Kyuhun Sim <kyuhunsim@gmail.com>
2022-07-11 14:52:14 +0900

    큐와 구현

---
+++ 1966.py
@@ -0,0 +1,27 @@
+a=int(input())
+
+for i in range(a):
+
+  m,n=map(int,input().split())
+
+  queue=list(map(int,input().split()))  
+  count=0
+  idx=[j for j in range(m)]
+
+  while queue:
+
+    if queue[0]==max(queue):
+      count+=1
+      if idx[0]==n:
+        print(count)
+        break
+      queue.pop(0)
+      idx.pop(0)
+        
+
+    else:
+      queue.append(queue.pop(0))
+      idx.append(idx.pop(0))
+      
+      
+    
